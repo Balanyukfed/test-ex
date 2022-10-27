c3429c649e91295bceaba71ef6bfafa8e04bc913
Balanyuk Nikita <NekitGraviton2002@gmail.com>
2022-10-27 20:10:24 +0300

    changed Ex

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+let separation = Array.from(document.querySelectorAll('.table__row>div'));
+
+window.onload = function(){
+    separation.forEach(block => {
+        block.classList.add('separation');
+        console.log('1');
+    });
+}
+
+let link = document.querySelectorAll('.nav>ul>li>a');
+
+for(let i = 0; i < link.length; i++){
+    link[i].addEventListener('click', ()=>{
+        link.forEach((el) => {
+            if(el.classList.contains('link_active')){
+                el.classList.remove('link_active');
+            }
+        });
+        
+        link[i].classList.toggle('link_active');
+        
+    })
+}
 
 let openModal = document.getElementById('openModal');
 let modal = document.getElementById('myModal');
@@ -5,7 +28,6 @@ let modalClose = document.querySelector('.modal__close-button');
 
 openModal.addEventListener('click', ()=>{
     modal.classList.add("modal-active");
-    console.log('1');
 })
 modalClose.addEventListener('click', ()=>{
     modal.classList.remove('modal-active');
